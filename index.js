2ac24ea71d09444b8a4337d559dbf5a86ad90164
Natasha K <kapr.natasha@gmail.com>
2018-07-01 20:35:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,8 @@ function prependKitten(name){
   var kittens2 = [name, ...kittens]
   return kittens2
 }
+
+function removeLastKitten(){
+  var kittens2 = kittens.slice(0, kittens.length - 1)
+  return kittens2
+}
