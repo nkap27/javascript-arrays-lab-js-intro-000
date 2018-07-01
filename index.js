7a3c7c2f4d3fb3e02a9fbd214829218164105747
Natasha K <kapr.natasha@gmail.com>
2018-07-01 20:29:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
-  return kittens.push("Ralph");
+  return kittens.push("Ralph")
 }
 
 function destructivelyPrependKitten(name){
